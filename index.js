55297360a5aad52922e04e42ac10c745985b86d1
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-11-10 11:23:42 -0600

    conectando servidor con express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('En linear');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola ya estamos en linea con express');
+})
+
+app.listen(port, () => {
+  console.log('Estoy en linea en el puesto: ' + port);
+});
