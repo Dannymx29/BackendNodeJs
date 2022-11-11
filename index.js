16899eba0f7003ff30df1728bf8e94af4b14b848
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-11-11 01:21:25 -0600

    Restful API

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola ya estamos en linea con express');
-})
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'producto 1',
+    price: 1000
+  });
+});
+
+app.get('/profile', (req, res) => {
+  res.send('My profile');
+});
 
 app.listen(port, () => {
   console.log('Estoy en linea en el puesto: ' + port);
