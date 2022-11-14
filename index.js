6d01f584fbdf15aaf584392d1a8383dfcaa89fd9
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-11-14 16:20:23 -0600

    added a procfile

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const routerApi = require('./src/routes');
 const { logErrors, errorHandler, boomErrorHandler} = require('./src/middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 routerApi(app);
 
-const whitelist = ['http://localhost:8080'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
